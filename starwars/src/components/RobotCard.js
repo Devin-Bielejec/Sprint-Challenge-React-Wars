09eecc5e5c7960bbf21f70f87e0bc29e9f3c771e
devin-bielejec <devin.bieleje@gmail.com>
2019-08-16 13:18:36 -0400

    Tried something in app but didnt' work

--- starwars/src/components/RobotCard.js
+++ starwars/src/components/RobotCard.js
@@ -1,6 +1,7 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import styled from "styled-components";
-import { Card } from "./CharacterCard";
+import { Card, Img } from "./CharacterCard";
+import axios from "axios";
 
 const Robot = styled(Card)`
     color: brown;
@@ -12,10 +13,19 @@ const RobotCard = (props) => {
     const hairColor = props.character["hair_color"];
     const eyeColor = props.character["eye_color"];
 
+    //set a default one, getting the other one from Upsplash API
+    const [pic, setPic] = useState(false);
+
+    useEffect( () => {
+        axios.get("https://source.unsplash.com/1600x900/?robot")
+        .then(res => setPic(res.request.responseURL))
+        .catch(err => console.log(`Error: ${err}`))
+    }, [pic])
+
     return(
         <Robot>
             <div className="image">
-                <img src="../images/Robot.jpg"></img>
+                <Img src={pic}></Img>
             </div>
             <h1>{name}</h1>
             <h3>{`I am a handsome ${gender} with gorgeous ${hairColor} and beautiful ${eyeColor} eyes.`}</h3>
